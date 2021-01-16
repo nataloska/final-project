6dde306675d7ddb6688f0794c46af03db5b5ace8
nataloska <natalia.drnkova@gmail.com>
2021-01-16 16:31:58 +0000

    product details added

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_info, name='product_info'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 # Create your views here.
 
 
 def all_products(request):
-    # view to return index page
+    # view to return all products
 
     products = Product.objects.all()
 
@@ -13,3 +13,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_info(request, product_id):
+    # view to return product details
+
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_info.html', context)
