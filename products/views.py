d1d207835f5e3367ca216f65bfcf0869bf041238
nataloska <natalia.drnkova@gmail.com>
2021-01-18 17:44:06 +0000

    sort start

--- products/views.py
+++ products/views.py
@@ -10,11 +10,11 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    category = None
+    categories = None
 
     if request.GET:
         if 'category' in request.GET:
-            categories = request.GET['category']
+            categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
